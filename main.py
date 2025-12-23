d694debd31003da08e93c73d9cef74d87cbd28c2
Chitransh196 <chitranshraizada@gmail.com>
2025-12-23 15:49:29 +0530

    corssssssss

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],      
+    allow_origins=[
+        "https://st-dt-frontent-new.onrender.com"
+    ],      
     allow_credentials=True,
     allow_methods=["*"],        
     allow_headers=["*"],
